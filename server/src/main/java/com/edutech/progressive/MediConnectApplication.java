c0af236c04baf7c272879247906943054aa0a4a9
AatishKamble <dummy@gmail.com>
2026-02-01 11:36:53 +0000

    Day 4 Task

--- server/src/main/java/com/edutech/progressive/MediConnectApplication.java
+++ server/src/main/java/com/edutech/progressive/MediConnectApplication.java
@@ -8,12 +8,16 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.edutech.progressive.config.DatabaseConnectionManager;
 import com.edutech.progressive.dao.PatientDAOImpl;
 import com.edutech.progressive.entity.Patient;
 import com.edutech.progressive.service.PatientService;
 import com.edutech.progressive.service.impl.PatientServiceImplJdbc;
 
+
+@SpringBootApplication
 public class MediConnectApplication {
     public static void main(String[] args) throws Exception {
 
